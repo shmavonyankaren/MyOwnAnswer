33f75b07538f787560b0899b06972cea884fff8a
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-06-19 18:16:18 +0400

    eleventh commit

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async(e) =>{
 
     loader(messageDiv);
 
-    const response = await fetch("https://myownanswers.onrender.com/", {
+    const response = await fetch("http://localhost:5000/", {
                         method: "POST",
                             headers: {
                                 "Content-Type": "application/json",
